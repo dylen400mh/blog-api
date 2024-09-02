4dc87e23deb35e3f0ae285e0f4c2ac578877e65d
dylen400mh <dylenbelanger2004@gmail.com>
2024-09-02 15:50:08 -0400

    pass req object to strategies

--- strategies/adminStrategy.js
+++ strategies/adminStrategy.js
@@ -3,11 +3,12 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.secret;
+opts.passReqToCallback = true;
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 // jwt strategy to only allow me to access protected routes
-module.exports = new JwtStrategy(opts, async (jwt_payload, done) => {
+module.exports = new JwtStrategy(opts, async (req, jwt_payload, done) => {
   try {
     const user = await prisma.user.findFirst({
       where: {
@@ -15,6 +16,7 @@ module.exports = new JwtStrategy(opts, async (jwt_payload, done) => {
       },
     });
     if (user && user.username === jwt_payload.username) {
+      req.user = user;
       return done(null, user);
     } else {
       return done(null, false);
--- strategies/userStrategy.js
+++ strategies/userStrategy.js
@@ -4,25 +4,27 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.secret;
+opts.passReqToCallback = true;
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const userStrategy = new JwtStrategy(opts, async (jwt_payload, done) => {
+const userStrategy = new JwtStrategy(opts, async (req, jwt_payload, done) => {
   try {
     const user = await prisma.user.findFirst({
       where: {
-        id: jwt_payload.id,  // Authenticate any valid user by ID from the JWT payload
+        id: jwt_payload.id, // Authenticate any valid user by ID from the JWT payload
       },
     });
 
     if (user) {
-      return done(null, user);  // Attach the authenticated user to req.user
+      req.user = user;
+      return done(null, user); // Attach the authenticated user to req.user
     } else {
-      return done(null, false);  // Authentication failed
+      return done(null, false); // Authentication failed
     }
   } catch (err) {
-    return done(err, false);  // Handle any errors
+    return done(err, false); // Handle any errors
   }
 });
 
