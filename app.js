0039bd1970931f4f543f3d87569ffa004a1fc487
dylen400mh <dylenbelanger2004@gmail.com>
2024-09-04 11:54:20 -0400

    Add CORS to app.js

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const usersRouter = require("./routes/usersRouter");
 const postsRouter = require("./routes/postsRouter");
 const commentsRouter = require("./routes/commentsRouter");
-
+const cors = require("cors");
 const app = express();
 
+corsOptions = {
+  origin: process.env.FRONTEND_ORIGIN,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,4 +16,4 @@ app.use("/api", usersRouter);
 app.use("/api/posts", postsRouter);
 app.use("/api/", commentsRouter);
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(3001, () => console.log("Listening on port 3001"));
