309cd989c4a96ed57590aa9bddb6e50681079e85
dylen400mh <dylenbelanger2004@gmail.com>
2024-09-06 10:55:51 -0400

    Add authentication middleware to new users route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,11 +1,19 @@
 const { Router } = require("express");
 const userController = require("../controllers/userController");
 const usersRouter = Router();
+const userStrategy = require("../strategies/userStrategy");
+const adminStrategy = require("../strategies/adminStrategy");
+const passport = require("passport");
+passport.use(adminStrategy, userStrategy);
 
 usersRouter.post("/register", userController.registerUser);
 usersRouter.post("/login", userController.loginUser);
 usersRouter.post("/logout", userController.logoutUser);
-usersRouter.get("/users", userController.getUsers);
+usersRouter.get(
+  "/users",
+  passport.authenticate("jwt", { session: false }),
+  userController.getUsers
+);
 usersRouter.get("/profile");
 usersRouter.put("/profile");
 usersRouter.delete("/profile");
