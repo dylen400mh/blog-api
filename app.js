b12f6cbea78ff8a6b9f4f8d190fc0bb68c8666c1
dylen400mh <dylenbelanger2004@gmail.com>
2024-09-07 16:36:22 -0400

    Switch to port 3000

--- app.js
+++ app.js
@@ -16,4 +16,4 @@ app.use("/api", usersRouter);
 app.use("/api/posts", postsRouter);
 app.use("/api/", commentsRouter);
 
-app.listen(3001, () => console.log("Listening on port 3001"));
+app.listen(3000, () => console.log("Listening on port 3000"));
