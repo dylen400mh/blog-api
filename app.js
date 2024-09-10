3092b219d11d74ad609f0bb3306abd50e40e3575
dylen400mh <dylenbelanger2004@gmail.com>
2024-09-10 13:58:52 -0400

    Add /send-email endpoint to send emails when contact form is submitted on portfolio

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const usersRouter = require("./routes/usersRouter");
 const postsRouter = require("./routes/postsRouter");
 const commentsRouter = require("./routes/commentsRouter");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 const app = express();
 
 corsOptions = {
@@ -15,5 +16,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", usersRouter);
 app.use("/api/posts", postsRouter);
 app.use("/api/", commentsRouter);
+
+// Handling emails from portfolio site
+const transporter = nodemailer.createTransport({
+  service: "outlook",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+app.use("/api/send-email", (req, res, next) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL,
+    subject: "New Message from Portfolio Site",
+    text: `${message}\n\n${name}`,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "Error sending message, please try again!" });
+    }
+    return res.status(200).json({ message: "Message sent, thanks!" });
+  });
+});
 
 app.listen(3000, () => console.log("Listening on port 3000"));
